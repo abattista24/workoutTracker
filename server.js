26f08c3ccf79ee9c21917eb45e7caaa2bfb4e922
Anna Battistals <anna.r.battista@gmail.com>
2020-11-16 19:50:37 -0700

    added api routes to server.js

--- server.js
+++ server.js
@@ -11,8 +11,11 @@
 //view stats
 
 
+const { WSAENOPROTOOPT } = require("constants");
 const express = require("express");
+const router =require("express").Router();
 const mongojs = require("mongojs");
+const mongoose = require("mongoose")
 const logger = require("morgan");
 const path = require("path");
 
@@ -26,14 +29,87 @@ app.use(express.json());
 app.use(express.static("public"));
 
 const databaseUrl = "fitnessTracker";
-const collections = ["workouts"];
+const collections = ["myWorkouts"];
 
 const db = mongojs(databaseUrl, collections);
+const workout = require("../models/workout.js");
+
 
 db.on("error", error => {
   console.log("Database Error:", error);
 });
 
+//-------------ROUTES--------------
+
+
+
+
+
+
+// ------API ROUTES -----------
+// look at last workout
+router.get("/api/workouts", (req,res) => {
+    workout.find({})
+    .then(db => {
+        res.json(db);
+    })
+    .if(err => {
+        res.send(err);
+    });
+});
+
+//for stats
+router.get("api/workouts/range", (req, res) => {
+    workout.find({}).limit(5)
+    .then(db => {
+        res.json;
+    })
+    .if(err => {
+        res.send(err);
+    });
+});
+
+
+//create new workout
+router.post("/api/workouts", (req, res) => {
+    var newWorkout = req.body
+    workout.create({})
+    .then(db => {
+        res.json(db)
+    })
+    .if(err =>
+        res.send(err))
+})
+
+
+//new excercise
+router.put("/api/workout/:id", (req, res) =>{
+    workout.findByIdAndUpdate(req.params.id, 
+        {$push: {exercises:req.body}},
+        {new:true, runValidators: true})
+    .then(db =>{
+        res,json(db);
+    })
+    .if(err => {
+        res.send(err);
+    });
+});
+
+
+//delete workout
+routerr.delete("/api/workouts", (req, res) =>{
+    workout.findByIdAndDelet(req.body.id)
+    .then(() => {
+        res.json(true);
+    })
+    .if(err => {
+        res.send(err);
+    });
+});
+
+
+
+//--------Mongo db functions--------
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "./public/index.html"));
 });
